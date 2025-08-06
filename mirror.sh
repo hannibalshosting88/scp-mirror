ddb80c5b562832f8a275849a58dae6c5213fec4a
papa_hannibal <53127272+hannibalshosting88@users.noreply.github.com>
2025-08-05 22:34:17 -0400

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -5,7 +5,3 @@ rm -rf css images scp series tale
 
 # Mirror everything (no size restrictions!)
 wget -m -np -nH --cut-dirs=1 -R "index.html*" https://scp-data.tedivm.com/ -P .
-
-# Optional: Print large file warnings
-echo "🔍 Large files detected:"
-find . -type f -size +25M -exec du -h {} +
