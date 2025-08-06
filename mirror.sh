abfd3e9ee8298bf104aecac96d2605567eff8c52
papa_hannibal <53127272+hannibalshosting88@users.noreply.github.com>
2025-08-05 21:16:57 -0400

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
+# Clean previous data
+rm -rf scp-data
 mkdir -p scp-data
+
+# Mirror everything except large files
 wget -m -np -nH --cut-dirs=1 -R "index.html*" https://scp-data.tedivm.com/ -P ./scp-data
+
+# Remove or move any file over 25MB (e.g. content_goi.json)
+find ./scp-data -type f -size +25M -delete
